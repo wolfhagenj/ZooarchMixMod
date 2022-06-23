979932db061b9695bd0626f3bb67ffe711f8c088
Jesse Wolfhagen <jl.wolfhagen@gmail.com>
2022-06-22 22:31:53 -0400

    moving data files

---
+++ Scripts/single_assemblage_mixmod.R
@@ -0,0 +1,9 @@
+####Data Cleaning Script####
+#This script is part of the ZooarchMixMod GitHub project (https://github.com/wolfhagenj/ZooarchMixMod)
+#It provides instructions to take standardized faunal measurements from a single assemblage and model
+#them as a mixture of 
+
+#This code is meant to be copied and adapted for your own personal use. While it is designed for data
+#set up like the OpenContext data used in the original manuscript, the same principles can be adapted
+#to data set up in a different way. The principles can even be used to standardize the data yourself
+#outside of R. This script helps with creating the necessary numeric codes that the Stan model expects.
